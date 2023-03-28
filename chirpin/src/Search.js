c91127c5264754dc6936caa88a595a94ff3e4e78
JIANGHongxu111 <1155141403@link.cuhk.edu.hk>
2023-03-28 16:22:39 +0800

    update search

---
+++ chirpin/src/Search.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Search extends React.Component{
+    render(){
+        return (
+        <div>search</div>
+        )
+    }
+}
+
+export { Search };
+    
