8c00bba3436d88c26ccb9eb74565f12f1fb4e211
dachangcishen <91406797+dachangcishen@users.noreply.github.com>
2023-03-29 20:35:36 +0800

    Add files via upload

--- chirpin/src/Chat.js
+++ chirpin/src/Chat.js
@@ -1,64 +1,83 @@
-import React from 'react';
-import TweetListView from './Tweet';
-import UserListView from './User';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import "./css/Chatbox.css"
+import React, { useState, useRef, useEffect } from 'react';
+import "./css/Chatbox.css";
 
-const chatBox = document.querySelector('.chat-box');
-const closeButton = document.querySelector('.close-button');
-const messageContainer = document.querySelector('.message-container');
-const messageInput = document.querySelector('.message-input');
-const sendButton = document.querySelector('.send-button');
+function ChatBox() {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const messageContainerRef = useRef(null);
 
-closeButton.addEventListener('click', () => {
-  chatBox.style.display = 'none';
-});
+  useEffect(() => {
+    messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
+  }, [messages]);
 
-sendButton.addEventListener('click', sendMessage);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-messageInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    sendMessage();
-  }
-});
+  const handleSendButtonClick = () => {
+    if (inputValue === '') {
+      return;
+    }
+    const newMessage = {
+      id: Date.now(),
+      text: inputValue,
+      sent: true,
+    };
+    setMessages((messages) => [...messages, newMessage]);
+    setInputValue('');
+  };
 
-function sendMessage() {
-  const message = messageInput.value;
-  if (message === '') {
-    return;
-  }
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('message');
-  messageElement.textContent = message;
-  messageContainer.appendChild(messageElement);
-  messageInput.value = '';
-  messageInput.focus();
-  messageContainer.scrollTop = messageContainer.scrollHeight;
-  messageElement.classList.add('sent');
+  const handleInputKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSendButtonClick();
+    }
+  };
+
+  const handleWithdrawButtonClick = (messageId) => {
+    setMessages((messages) =>
+      messages.filter((message) => message.id !== messageId)
+    );
+  };
+
+  return (
+    <div className="chat-box">
+      <div className="chat-header">
+        <h3>Chat</h3>
+        <button className="close-button">&times;</button>
+      </div>
+      <div className="chat-body" ref={messageContainerRef}>
+        {messages.map((message) => (
+          <div
+            key={message.id}
+            className={`message ${message.sent ? 'sent' : ''}`}
+          >
+            {message.text}
+            {message.sent && (
+              <button
+                className="withdraw-button"
+                onClick={() => handleWithdrawButtonClick(message.id)}
+              >
+                ❌
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          className="message-input"
+          placeholder="Type your message..."
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={handleInputKeyPress}
+        />
+        <button className="send-button" onClick={handleSendButtonClick}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
 }
 
-
-function chat(){
-    return(
-        <>
-        <div class="chat-box">
-        <div class="chat-header">
-            <h3>Chat</h3>
-            <span class="close-button">&times;</span>
-        </div>
-        <div class="chat-body">
-            <div class="message-container"></div>
-        </div>
-        <div class="chat-footer">
-            <input type="text" class="message-input" placeholder="Type your message...">
-            </input>
-            <button class="send-button">Send</button>
-        </div>
-        </div>
-        </>
-    )
-}
-
-export default chat;
+export default ChatBox;
