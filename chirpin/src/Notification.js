0e1dc4a7d96aeb931a03928fdd57e46ab3e7227e
SuviAbs <85143629+SuviAbs@users.noreply.github.com>
2023-03-30 21:23:43 +0800

    fix: change icon

--- chirpin/src/Notification.js
+++ chirpin/src/Notification.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Link} from "react-router-dom";
-import MaterialIcon, {colorPalette} from 'material-icons-react';
+// import MaterialIcon, {colorPalette} from 'material-icons-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown, faComment, faRetweet, faUser} from '@fortawesome/free-solid-svg-icons';
+
 
 
 const data = [
-    {icon:"thumb_up", action: "liked your tweet", name:"CSCI1", time:"3 min", content:'"laalall"'},
-    {icon:"thumb_down", action: "disliked your tweet", name:"CSCI2", time:"3 min", content:'"lalalal"'},
-    {icon:"person", action: "started following you", name:"CSCI3", time:"3 min", content:''},
-    {icon:"chat", action: "commented on your tweet", name:"CSCI4", time:"3 min", content:'"lalalal"'},
-    {icon:"forward", action: "retweeted your tweet", name:"CSCI5", time:"3 min", content: '"alalal"'},
+    {icon:faThumbsUp, action: "liked your tweet", name:"CSCI1", time:"3 min", content:'"laalall"', potrait:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"},
+    {icon:faThumbsDown, action: "disliked your tweet", name:"CSCI2", time:"3 min", content:'"lalalal"', potrait:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"},
+    {icon:faUser, action: "started following you", name:"CSCI3", time:"3 min", content:'', potrait:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"},
+    {icon:faComment, action: "commented on your tweet", name:"CSCI4", time:"3 min", content:'"lalalal"', potrait:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"},
+    {icon:faRetweet, action: "retweeted your tweet", name:"CSCI5", time:"3 min", content: '"alalal"', potrait:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"},
 ]
 
 
@@ -47,7 +50,7 @@ class NotificationListView extends React.Component{
     render(){
         return(
             <div>
-                {data.map((note,index)=><SingleNotification key={index} icon={note.icon} action={note.action} name={note.name} time={note.time} content={note.content}/>)}
+                {data.map((note,index)=><SingleNotification key={index} icon={note.icon} action={note.action} name={note.name} time={note.time} content={note.content} potrait={note.potrait}/>)}
             </div>
         )
     }
@@ -61,8 +64,8 @@ class SingleNotification extends React.Component{
         return(
             <div class="card col-12">
                 <div class="card-body">
-                    <div className="d-inline-block m-2"> <MaterialIcon icon={this.props.icon} size='small'/> </div>
-                    <img class="img d-inline-block m-2" style={{width:"50px", height: "50px"}} src="download.png" alt="Card image cap"/>
+                    <div className="d-inline-block m-2"> <FontAwesomeIcon color='grey' icon={this.props.icon} size='small'/> </div>
+                    <img class="img d-inline-block m-2 rounded-circle" style={{width:"50px", height: "50px"}} src={this.props.potrait} alt="Card image cap"/>
                     <p class="card-text d-inline-block m-2">{this.props.name} {this.props.action} {this.props.content}</p>
                     <p class="card-text"><small class="text-muted">Last updated {this.props.time} ago</small></p>
                 </div>
