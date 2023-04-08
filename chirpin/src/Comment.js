7fad023474eb09fbace0ba9be19c914bb6761209
SuviAbs <85143629+SuviAbs@users.noreply.github.com>
2023-04-09 01:14:01 +0800

    feat: add reply

--- chirpin/src/Comment.js
+++ chirpin/src/Comment.js
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 import {commentExample} from "./Example";
 import {faStairs, FaStairs} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
+import {BACK_END} from './App';
+import {getLoginInfo} from './Login';
 
 class Comment extends React.Component{
     constructor(props){
@@ -28,7 +28,7 @@ class Comment extends React.Component{
                     </div>
                     <small className="opacity-50 text-nowrap">{this.props.time}</small>
                 </div>
-                <CommentForm floor={this.props.floor}/>
+                <CommentForm floor={this.props.floor} tid={this.props.tid} addComment = {this.props.addComment}/>
                 
             </div>
         )
@@ -36,11 +36,43 @@ class Comment extends React.Component{
 }
 
 class CommentList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            comments:commentExample
+        }
+        this.addComment = this.addComment.bind(this);
+    }
+
+    async addReply(clicked_floor){
+        let newCom = {
+            content: "Re floor "+clicked_floor+": "+document.getElementById('new-comment').value,
+            username: getLoginInfo().username,
+            tid:this.props.tid,
+            floor_reply: clicked_floor
+        };
+        console.log(newCom);
+        let com = await fetch(BACK_END + 'tweet/reply', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newCom),
+        });
+        let com_res = await com.json();
+        console.log(com_res);
+        let new_comments = this.state.comments;
+        new_comments.push({floor: com_res.floor, username: com_res.username, content:com_res.content, potrait: com_res.potrait, time: "Just now"});
+        this.setState({comments: new_comments});
+        console.log(this.state.comments);
+        document.getElementById('new-comment').value='';
+    }
+
     render(){
         return(
             <div className="list-group w-auto">
-                {commentExample.map((comment,index)=>
-                    <Comment key={index} name={comment.name} content={comment.content} potrait={comment.potrait} time={comment.time} floor={comment.floor}/>
+                {this.state.comments.map((comment,index)=>
+                    <Comment addComment = {this.addComment.bind(this)} key={index} name={comment.username} content={comment.content} potrait={comment.potrait} time={comment.time} floor={comment.floor} tid={this.props.tid}/>
                 )}
             </div>
         )
@@ -53,6 +85,7 @@ class CommentForm extends React.Component{
         super(props);
         console.log(this.props.floor)
     }
+
     render(){
         return(
             <div class="modal fade" id={"commentForm"+this.props.floor} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -63,11 +96,11 @@ class CommentForm extends React.Component{
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <textarea className="form-control" id='new-comment' rows='5'></textarea>
+                            <textarea className="form-control" id={'new-comment'} rows='5'></textarea>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"> Cancel </button>
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal"> Send </button>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={()=>this.props.addReply(this.props.floor)}> Send </button>
                         </div>
                     </div>
                 </div>
