93e0e0b5131a38b7c40c8081cf1d95e542337888
Lucas-hao <843691596@qq.com>
2023-04-10 22:46:06 +0800

    fix tweet and user bugs

--- chirpin/src/Tweet.js
+++ chirpin/src/Tweet.js
@@ -51,7 +51,7 @@ function TweetCard({ tweetInfo, addComment, isDetailPage = true }) {
   const tweetContent = tweetInfo['content'];
   const portraitUrl = tweetInfo['portraitUrl'];
   const tags = tweetInfo['tags'];
-  
+
 
   // update time interval every second
   useEffect(() => {
@@ -184,36 +184,40 @@ function TweetCard({ tweetInfo, addComment, isDetailPage = true }) {
                   <button type="button" className="btn btn-primary btn-floating">View Full Tweet</button>
                 </Link>
               </span>}
-              <span className="m-1">
-                <button type="button" className={"btn btn-" + (likeInfo.bLikeByUser ? "" : "outline-") + "primary btn-floating"} onClick={clickLikeTweet}>
-                  <FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon>
-                </button>
-                <span className="ms-1 opacity-75">{likeInfo.likeCount}</span>
-              </span>
-              <span className="m-1">
-                <button type="button" className={"btn btn-" + (dislikeInfo.bDislikeByUser ? "" : "outline-") + "primary btn-floating"} onClick={clickDislikeTweet}>
-                  <FontAwesomeIcon icon={faThumbsDown}></FontAwesomeIcon>
-                </button>
-                <span className="ms-1 opacity-75">{dislikeInfo.dislikeCount}</span>
-              </span>
-              <span className="m-1">
+              {getLoginInfo() && getLoginInfo()['mode'] == 'user' &&
+                <>
+                  <span className="m-1">
+                    <button type="button" className={"btn btn-" + (likeInfo.bLikeByUser ? "" : "outline-") + "primary btn-floating"} onClick={clickLikeTweet}>
+                      <FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon>
+                    </button>
+                    <span className="ms-1 opacity-75">{likeInfo.likeCount}</span>
+                  </span>
+                  <span className="m-1">
+                    <button type="button" className={"btn btn-" + (dislikeInfo.bDislikeByUser ? "" : "outline-") + "primary btn-floating"} onClick={clickDislikeTweet}>
+                      <FontAwesomeIcon icon={faThumbsDown}></FontAwesomeIcon>
+                    </button>
+                    <span className="ms-1 opacity-75">{dislikeInfo.dislikeCount}</span>
+                  </span>
+                  <span className="m-1">
 
-                <button type="button" className="btn btn-outline-primary btn-floating" data-bs-toggle="modal" data-bs-target="#tweetCommentForm" data-bs-whatever="@mdo">
-                  <FontAwesomeIcon icon={faComment}></FontAwesomeIcon>
-                </button>
-                <span className="ms-1 opacity-75">{commentCount}</span>
-              </span>
-              <span className="m-1">
-                <a className="btn btn-outline-primary btn-floating"  href="#tweetForwardForm" data-bs-toggle="modal" role='button'>
-                  <FontAwesomeIcon icon={faRetweet}></FontAwesomeIcon>
-                </a>
-                <span className="ms-1 opacity-75">{retweetCount}</span>
-              </span>
-              <span className="m-1">
-                <button type="button" className={"btn btn-floating" + (isReported ? "btn-primary disabled" : " btn-outline-primary")} data-bs-toggle="modal" data-bs-target="#report-popup">
-                  <FontAwesomeIcon icon={faWarning}></FontAwesomeIcon>
-                </button>
-              </span>
+                    <button type="button" className="btn btn-outline-primary btn-floating" data-bs-toggle="modal" data-bs-target="#tweetCommentForm" data-bs-whatever="@mdo">
+                      <FontAwesomeIcon icon={faComment}></FontAwesomeIcon>
+                    </button>
+                    <span className="ms-1 opacity-75">{commentCount}</span>
+                  </span>
+                  <span className="m-1">
+                    <a className="btn btn-outline-primary btn-floating" href="#tweetForwardForm" data-bs-toggle="modal" role='button'>
+                      <FontAwesomeIcon icon={faRetweet}></FontAwesomeIcon>
+                    </a>
+                    <span className="ms-1 opacity-75">{retweetCount}</span>
+                  </span>
+                  <span className="m-1">
+                    <button type="button" className={"btn btn-floating" + (isReported ? "btn-primary disabled" : " btn-outline-primary")} data-bs-toggle="modal" data-bs-target="#report-popup">
+                      <FontAwesomeIcon icon={faWarning}></FontAwesomeIcon>
+                    </button>
+                  </span>
+                </>
+              }
             </div>
           </div>
         </div>
@@ -258,7 +262,7 @@ function TweetCard({ tweetInfo, addComment, isDetailPage = true }) {
       </div>
 
       {/* forward tweet */}
-      <ForwardForm tid={tweetInfo.tid}/>
+      <ForwardForm tid={tweetInfo.tid} />
 
       {/* forward select tag*/}
 
@@ -320,7 +324,7 @@ function ForwardForm(tid) {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(postBody)  
+        body: JSON.stringify(postBody)
       }).then(res => {
         console.log(res)
         if (res.status === 201) {
@@ -443,7 +447,7 @@ function ForwardForm(tid) {
       </div>
 
       {/* modal for choosing tags*/}
-      {/* <div className="modal fade" id="add-tag-retweet" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+      <div className="modal fade" id="add-tag-retweet" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
@@ -465,11 +469,11 @@ function ForwardForm(tid) {
               </div>
             </div>
             <div className="modal-footer">
-              <button type="button" id="close-modal" className="btn btn-secondary" data-bs-target="#tweetForwardForm" data-bs-toggle="modal" data-bs-dismiss="modal">Back</button>
+              <button type="button" id="close-modal" className="btn btn-secondary" data-bs-target="#tweetForwardForm" data-bs-toggle="modal-back" data-bs-dismiss="modal-back">Back</button>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
